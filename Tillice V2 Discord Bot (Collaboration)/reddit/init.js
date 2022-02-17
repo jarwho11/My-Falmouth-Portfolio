a247db57dbd1dff61b3fab6b062680c2210cd999
jarwho11 <40441179+jarwho11@users.noreply.github.com>
2022-02-17 13:45:00 +0000

    tillice v2 part 2
    didnt let me upload everything at once

---
+++ Tillice V2 Discord Bot (Collaboration)/reddit/init.js
@@ -0,0 +1,14 @@
+const snoowrap = require('snoowrap')
+
+// Initiate a reddit client application (Script type)
+const redditClient = new snoowrap({
+    userAgent: 'Tilice V2 from r/flairwars',
+    clientId: process.env.REDDIT_CLIENTID,
+    clientSecret: process.env.REDDIT_CLIENTSECRET,
+    username: process.env.REDDIT_USERNAME,
+    password: process.env.REDDIT_PASSWORD
+})
+
+redditClient.config({ continueAfterRatelimitError: true })
+
+module.exports = redditClient
